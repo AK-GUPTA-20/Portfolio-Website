233aa1ce156999b3fc8437f90b60fcfc869d500e
AK-GUPTA-20 <guptaakshat7795@gmail.com>
2025-08-22 16:17:59 +0530

    commit-01

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('.text', {
+        strings: ["Data Scientist", "Backend Developer", "Python Programmer", "Problem Solver", "Java Enthusiast", "C Programmer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
